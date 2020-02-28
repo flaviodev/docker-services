18ca8e07c18316585c1efa848e3e8038e126da6e
flaviodev <fdsdev@gmail.com>
2020-02-28 18:59:08 -0300

    rename file

--- postgres/10.12/extract_pg_data_to_volume.sh
+++ postgres/10.12/extract_pg_data_to_volume.sh
@@ -3,4 +3,6 @@ echo 'copying postgres 10.12 volume from container'
 
 mkdir -p volume/postgresql
 
-docker cp pg10:/var/lib/postgresql/data ~/git/docker/postgres/10.12/volume/postgresql
+CURRENT_PATH=`pwd`
+
+docker cp pg10:/var/lib/postgresql/data $CURRENT_PATH/volume/postgresql
